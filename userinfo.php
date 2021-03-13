0b68ffa1755b8d110cfeae534a8c4d4a7cceceda
TA Sakin <tasakin98@gmail.com>
2021-03-13 20:53:25 +0600

    IslamicInfo_Website

---
+++ userinfo.php
@@ -0,0 +1,23 @@
+<?php
+$con = mysqli_connect('localhost','root');
+if ($con) {
+  echo "Conenction successful";
+}
+else{
+  echo "No connection";
+}
+
+mysqli_select_db($con, 'WebsiteData');
+$user = $_POST['user'];
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+
+
+$query = "insert into userinfodata(user, email, pass)
+values('$user', '$email', '$pass')";
+
+echo "$query";
+mysqli_query($con, $query);
+header('location:index.html');
+
+?>
